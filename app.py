a445187b008f6c2f3b814da5b2342356bb47ff66
Matt Grochocinski <matt.grochocinski@gmail.com>
2021-12-09 10:44:43 -0600

    oopsie

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def collage(image_paths):
             y += single_height
         x += single_width+divider
         y = 0
-    out.paste(versus,((single_width*cols+divider)-88/2, (single_height*rows)-75/2), versus)
+    out.paste(versus,(int((single_width*cols+divider)-88/2), int((single_height*rows)-75/2)), versus)
     out.save("Collage.jpg")
 
 
