dee60d26b6bd66226c44399007bd07ee9642e7ca
Bill Hanson <doubleyuhtee@gmail.com>
2021-12-09 09:41:35 -0600

    Hero collage, needs error handling

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import logging
 import requests
 import random
+from PIL import Image
 
 
 logger = logging.getLogger('discord')
@@ -33,7 +34,7 @@ def get_hero_info():
 
 
 def get_hero_img(hero_data):
-    img_name = hero_data['name'].replace('npc_dota_hero_', '') + "_sb.png"
+    img_name = hero_data['name'].replace('npc_dota_hero_', '') + "_lg.png"
     save_location = f"imagecache/{img_name}"
     if exists(save_location):
         print("Found")
@@ -46,6 +47,28 @@ def get_hero_img(hero_data):
                 if not block:
                     break
                 handle.write(block)
+    return save_location
+
+
+def collage(image_paths):
+    hero_imgs = [Image.open(x) for x in image_paths]
+    cols = 2
+    rows = 3
+    single_height = hero_imgs[0].height
+    single_width = hero_imgs[0].width
+    out = Image.new('RGB', (single_width*cols+5, single_height*rows))
+    i = 0
+    x = 0
+    y = 0
+    for col in range(cols):
+        for row in range(rows):
+            print(i, x, y)
+            out.paste(hero_imgs[i], (x, y))
+            i += 1
+            y += single_height
+        x += single_width+5
+        y = 0
+    out.save("Collage.jpg")
 
 
 @client.event
@@ -71,18 +94,21 @@ async def on_message(message):
                       {'localized_name': "hero6"},
                       ]
         chosen = []
+        images = []
         for i in range(0,6):
             pick = random.choice(heroes)
             chosen.append(pick)
-            get_hero_img(pick)
+            images.append(get_hero_img(pick))
+        collage(images)
         embedVar=discord.Embed(
             title="Let's Get Ready to Street Dota!",
             color=0xaf0101)
-        # embedVar.set_image(url=img_url)
+        file = discord.File("Collage.jpg", filename="image.jpg")
+        embedVar.set_image(url="attachment://image.jpg")
         embedVar.add_field(name="Radiant Heroes", value=f"{chosen[0]['localized_name']}\n{chosen[1]['localized_name']}\n{chosen[2]['localized_name']}", inline=True)
         embedVar.add_field(name="Dire Heroes", value=f"{chosen[3]['localized_name']}\n{chosen[4]['localized_name']}\n{chosen[5]['localized_name']}", inline=True)
         embedVar.set_footer(text="Game started by: {}".format(message.author))
-        msg = await message.channel.send(embed=embedVar)
+        msg = await message.channel.send(file=file, embed=embedVar)
         await msg.add_reaction("<:morphGive:908107050163249272>")
 
 
