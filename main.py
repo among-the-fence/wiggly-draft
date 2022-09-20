44100e362ca950c626294d171de602a94dd41ce8
Bill Hanson <wt@doubleyuhtee.com>
2022-09-19 21:08:26 -0500

    recording users

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import random
 import discord
 import os
 from dotenv import load_dotenv
@@ -16,6 +18,7 @@ env = {
     }
 }
 
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
@@ -30,18 +33,51 @@ class MyView(discord.ui.View):
 
     @discord.ui.button(label="Do", row=0, style=discord.ButtonStyle.primary)
     async def first_button_callback(self, button, interaction):
-        await interaction.response.send_message("You pressed me!")
+        messageid = self.message.id
+        if messageid not in bunches:
+            bunches[messageid] = set(())
+        user = interaction.user.display_name
+        if os.getenv('ENV') == 'DEV':
+            bunches[messageid].add(user + "1")
+            bunches[messageid].add(user + "2")
+            bunches[messageid].add(user + "3")
+            bunches[messageid].add(user + "4")
+            bunches[messageid].add(user + '5')
 
-    @discord.ui.button(label="NO", row=0, style=discord.ButtonStyle.danger)
+        bunches[messageid].add(user)
+        new_message = "Who's in?\n" + ', '.join(bunches[messageid])
+
+        if len(bunches[messageid]) >= 6:
+            self.timeout = None
+            for child in self.children:
+                child.disabled = True
+            self.children = {}
+            if len(bunches[messageid]) > 6:
+                new_message = "Too many presses. \n" + ', '.join(bunches[messageid])
+        await self.message.edit(content=new_message, view=self)
+
+        if len(bunches[messageid]) == 6:
+            await interaction.response.send_message(f"{','.join(bunches[messageid])} pressed me!")
+            bunches.clear()
+
+        print("done")
+
+
+    @discord.ui.button(label="No", row=0, style=discord.ButtonStyle.danger)
     async def second_button_callback(self, button, interaction):
         for child in self.children:
             child.disabled = True
         self.children = []
-        await self.message.edit(content="Don't do a hit!", view=self)
+        messageid = self.message.id
+        if messageid in bunches:
+            bunches[messageid] = None
+        user = interaction.user.display_name
+        await self.message.edit(content=f"{user} put a stop to it.", view=self)
 
-@bot.slash_command(name="wiggle", description="Make it")
+
+@bot.slash_command(name="wiggle", description="Time for street DOTA")
 async def hello(ctx):
-    await ctx.respond("Hey!", view=MyView(timeout=get_env_attribute('timeout')))
+    await ctx.respond("Who's in?", view=MyView(timeout=get_env_attribute('timeout')))
 
 
 def get_env_attribute(attribute):
