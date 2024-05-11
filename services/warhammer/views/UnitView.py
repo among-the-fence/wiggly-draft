b213b96927eab8ead2dd2b594e422bf2b1579676
Bill Hanson <wt@doubleyuhtee.com>
2024-05-11 09:57:29 -0500

    I expected something different.

--- services/warhammer/views/UnitView.py
+++ services/warhammer/views/UnitView.py
@@ -1,6 +1,7 @@
 import copy
 
 import discord
+from discord import Color
 
 from services.warhammer import Warhammer
 from util.utils import simple_format, send_in_chunks, extract_and_clear
@@ -26,64 +27,77 @@ class UnitView(discord.ui.View):
             e2 = discord.Embed(title=display, color=color, description=t)
             await interaction.respond(embed=e2)
 
+    async def handle_error(self, interaction, e):
+        e2 = discord.Embed(title="Error", description=f"{type(e)}  {e}")
+        await interaction.respond(embed=e2, ephemeral=True)
+
     @discord.ui.button(label="", style=discord.ButtonStyle.primary, emoji="🔫")
     async def gun_button_callback(self, button, interaction):
-        err, unit, color = self.get_unit()
-        e = discord.Embed(title="Ranged", color=color)
-        prop_order = [
-            {"key": "range", "display": "Ra"},
-            {"key": "attacks", "display": "At"},
-            {"key": "skill", "display": "Sk"},
-            {"key": "strength", "display": "St"},
-            {"key": "ap", "display": "AP"},
-            {"key": "damage", "display": "D"},
-            {"key": "keywords", "display": "K"}
-        ]
-        for x in unit.rangedWeapons:
-            for p in x["profiles"]:
-                pn = p["name"]
-                extract_and_clear(p, "name")
-                out = ""
-                for x in prop_order:
-                    if x["key"] in p:
-                        out += f"{x['display']}:**{p[x['key']]}** "
+        try:
+            err, unit, color = self.get_unit()
+            e = discord.Embed(title="Ranged", color=color)
+            prop_order = [
+                {"key": "range", "display": "Ra"},
+                {"key": "attacks", "display": "At"},
+                {"key": "skill", "display": "Sk"},
+                {"key": "strength", "display": "St"},
+                {"key": "ap", "display": "AP"},
+                {"key": "damage", "display": "D"},
+                {"key": "keywords", "display": "K"}
+            ]
+            for x in unit.rangedWeapons:
+                for p in x["profilesdfs"]:
+                    pn = p["name"]
+                    extract_and_clear(p, "name")
+                    out = ""
+                    for x in prop_order:
+                        if x["key"] in p:
+                            out += f"{x['display']}:**{p[x['key']]}** "
 
-                e.add_field(name=pn, value=out, inline=False)
-        await interaction.respond(embed=e)
+                    e.add_field(name=pn, value=out, inline=False)
+            await interaction.respond(embed=e)
+        except Exception as e:
+            await self.handle_error(interaction, e)
 
     @discord.ui.button(label="", style=discord.ButtonStyle.primary, emoji="⚔️")
     async def melee_button_callback(self, button, interaction):
+        try:
+            err, unit, color = self.get_unit()
+            e = discord.Embed(title="Melee", color=color)
+            prop_order = [
+                {"key": "range", "display": "Ra"},
+                {"key": "attacks", "display": "At"},
+                {"key": "skill", "display": "Sk"},
+                {"key": "strength", "display": "St"},
+                {"key": "ap", "display": "AP"},
+                {"key": "damage", "display": "D"},
+                {"key": "keywords", "display": "K"}
+            ]
+            for x in unit.meleeWeapons:
+                for p in x["profiles"]:
+                    pn = p["name"]
+                    extract_and_clear(p, "name")
+                    out = ""
+                    for x in prop_order:
+                        if x["key"] in p:
+                            out += f"{x['display']}:**{p[x['key']]}** "
 
-        err, unit, color = self.get_unit()
-        e = discord.Embed(title="Melee", color=color)
-        prop_order = [
-            {"key": "range", "display": "Ra"},
-            {"key": "attacks", "display": "At"},
-            {"key": "skill", "display": "Sk"},
-            {"key": "strength", "display": "St"},
-            {"key": "ap", "display": "AP"},
-            {"key": "damage", "display": "D"},
-            {"key": "keywords", "display": "K"}
-        ]
-        for x in unit.meleeWeapons:
-            for p in x["profiles"]:
-                pn = p["name"]
-                extract_and_clear(p, "name")
-                out = ""
-                for x in prop_order:
-                    if x["key"] in p:
-                        out += f"{x['display']}:**{p[x['key']]}** "
-
-                e.add_field(name=pn, value=out, inline=False)
-        await interaction.respond(embed=e)
+                    e.add_field(name=pn, value=out, inline=False)
+            await interaction.respond(embed=e)
+        except Exception as e:
+            await self.handle_error(interaction, e)
 
     @discord.ui.button(label="", style=discord.ButtonStyle.primary, emoji="🗿")
     async def ability_button_callback(self, button, interaction):
-        await self.respond_with(interaction, "Abilities", "abilities")
-
-
+        try:
+            await self.respond_with(interaction, "Abilities", "abilities")
+        except Exception as e:
+            await self.handle_error(interaction, e)
 
     @discord.ui.button(label="", style=discord.ButtonStyle.primary, emoji="☁️")
     async def fluff_button_callback(self, button, interaction):
-        await self.respond_with(interaction, "Fluff", "fluff")
+        try:
+            await self.respond_with(interaction, "Fluff", "fluff")
+        except Exception as e:
+            await self.handle_error(interaction, e)
 
