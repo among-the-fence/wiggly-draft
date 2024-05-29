b2f644a4678653ba45f6b1036149ecd5a18082a9
Bill Hanson <wt@doubleyuhtee.com>
2024-05-29 06:41:34 -0500

    Fixed a little bug...

--- services/warhammer/models/search_item.py
+++ services/warhammer/models/search_item.py
@@ -51,22 +51,33 @@ class SearchItem:
             match &= self.search_type(unit)
         return match
 
+    @staticmethod
+    def flatten(lst):
+        print(lst)
+        out = []
+        for i in lst:
+            if i:
+                out.extend(i)
+        print(out)
+        return out
+
     def filter(self, units: list[WHUnit]):
         prop_name = None
         extreme_value = -1
         if self.max_filter:
             prop_name = self.max_filter
-            flatten_values = [x.get_prop(self.max_filter) for x in units]
+            flatten_values = SearchItem.flatten([x.get_prop(self.max_filter) for x in units])
             extreme_value = max(flatten_values)
         if self.min_filter:
             prop_name = self.min_filter
-            flatten_values = [x.get_prop(self.max_filter) for x in units]
-            extreme_value = max(flatten_values)
+            flatten_values = SearchItem.flatten([x.get_prop(self.min_filter) for x in units])
+            extreme_value = min(flatten_values)
         out = []
         if prop_name:
             if type(extreme_value) is list:
                 extreme_value = extreme_value[0]
+            print(extreme_value)
             for u in units:
-                if extreme_value in u.get_prop(self.max_filter):
+                if extreme_value in u.get_prop(prop_name):
                     out.append(u)
         return out
