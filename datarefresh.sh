0b4e847fab3ee2baff85a68ddf8fecf67c76e74f
Bill Hanson <wt@doubleyuhtee.com>
2024-05-30 13:01:07 -0500

    simple

--- datarefresh.sh
+++ datarefresh.sh
@@ -3,9 +3,13 @@ mkdir data
 pushd data
 git clone --depth 1 https://github.com/game-datacards/datasources.git
 git clone --depth 1 https://github.com/BSData/wh40k-10e.git
+
+for d in */ ; do
+  echo $d.git*
+  rm -rf $d.git*
+done
+
 pushd datasources
-rm -rf .git*
-popd
-pushd wh40k-10e
-rm -rf .git*
-popd
+rm -rf 40k
+pushd 10th
+rm -rf conversion
