f349823bda836558ec331b1c1245edcfb6008e96
Bill Hanson <wt@doubleyuhtee.com>
2024-05-30 12:41:50 -0500

    remove all git stuff from sub modules

--- datarefresh.sh
+++ datarefresh.sh
@@ -4,8 +4,8 @@ pushd data
 git clone --depth 1 https://github.com/game-datacards/datasources.git
 git clone --depth 1 https://github.com/BSData/wh40k-10e.git
 pushd datasources
-rm -rf .git/*
+rm -rf .git*
 popd
 pushd wh40k-10e
-rm -rf .git/*
+rm -rf .git*
 popd
